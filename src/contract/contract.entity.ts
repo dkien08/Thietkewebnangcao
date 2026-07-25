74f0a27aae92c5e48db302ba0778a82f9de1d813
Namkhanhle <24100278@st.phenikaa-uni.edu.vn>
2026-07-25 15:19:25 +0000

    hoan thanh api va test cho contract

--- src/contract/contract.entity.ts
+++ src/contract/contract.entity.ts
@@ -1,25 +1,57 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Room } from '../room/room.entity';
+
+export enum ContractStatus {
+  PENDING = 'Pending',
+  ACTIVE = 'Active',
+  REJECTED = 'Rejected',
+  EXPIRED = 'Expired',
+  TERMINATED = 'Terminated',
+}
+
 
 @Entity('contracts')
 export class Contract {
   @PrimaryGeneratedColumn()
-  id!: number;
+  id: number;
 
+  
   @Column({ name: 'tenant_id' })
-  tenantId!: number; // ID của khách thuê (User)
+  tenantId: number;
 
   @Column({ name: 'room_id' })
-  roomId!: number; // ID của phòng trọ (Room)
+  roomId: number;
 
-  @Column({ type: 'date' })
-  startDate!: string; // Ngày bắt đầu hợp đồng
+  @Column()
+  startDate: Date;
 
-  @Column({ type: 'date' })
-  endDate!: string; // Ngày kết thúc hợp đồng
+  @Column()
+  endDate: Date;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
-  price!: number; // Giá thuê thỏa thuận trên hợp đồng
+  @Column('decimal')
+  price: number;
 
-  @Column({ type: 'enum', enum: ['Active', 'Expired', 'Terminated'], default: 'Active' })
-  status!: string; // Trạng thái hợp đồng
+  @Column({
+    type: 'enum',
+    enum: ContractStatus,
+    default: ContractStatus.PENDING,
+  })
+  status: ContractStatus;
+
+  // 3. Khai báo ánh xạ cho createdAt và updatedAt
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  // --- QUAN HỆ (RELATIONS) ---
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'tenant_id' }) 
+  tenant: User;
+
+  @ManyToOne(() => Room)
+  @JoinColumn({ name: 'room_id' }) 
+  room: Room;
 }
