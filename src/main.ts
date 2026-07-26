02883e4cabb520e092f52714f20558fbf0bd8d95
Dang Duc Kien <24100323@st.phenikaa-uni.edu.vn>
2026-07-26 15:22:48 +0700

    them host de render deploy

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import cookieParser from "cookie-parser";
-import helmet from 'helmet';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,15 +10,19 @@ async function bootstrap() {
   app.enableCors({
     origin: (origin, callback) => {
       // Cho phép request không có origin (like mobile apps/postman) hoặc đúng domain
-      if (!origin || origin.includes('app.github.dev') || origin.includes('localhost')) {
+      if (
+        !origin ||
+        origin.includes("app.github.dev") ||
+        origin.includes("localhost")
+      ) {
         callback(null, true);
       } else {
         callback(null, true); // Chấp nhận tất cả domain gửi kèm credentials
       }
     },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
+    allowedHeaders: "Content-Type, Accept, Authorization",
   });
 
   app.use(cookieParser());
@@ -27,13 +31,13 @@ async function bootstrap() {
   app.use(
     helmet({
       crossOriginResourcePolicy: { policy: "cross-origin" },
-    })
+    }),
   );
 
   app.setGlobalPrefix("api");
 
   const port = process.env.PORT || 3001;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 
   console.log(`🚀 Server đang chạy tại: http://localhost:${port}/api`);
 }
